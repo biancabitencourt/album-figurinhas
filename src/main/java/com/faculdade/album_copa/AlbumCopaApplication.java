30d1e3616dcfc3c880af1c77e49c5d7b69263db1
biancabitencourt <biancabitencourt03@gmail.com>
2026-05-29 11:40:21 -0300

    Commit 1: projeto inicial Spring Initializr

---
+++ src/main/java/com/faculdade/album_copa/AlbumCopaApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.album_copa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumCopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumCopaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faculdade/album_copa/AlbumCopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculdade.album_copa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumCopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
