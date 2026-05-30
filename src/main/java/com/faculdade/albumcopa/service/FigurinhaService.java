4ac6b1dcfb43f6b89edcf9e3578a700e82335ae2
biancabitencourt <biancabitencourt03@gmail.com>
2026-05-30 16:58:42 -0300

    Service

---
+++ src/main/java/com/faculdade/albumcopa/service/FigurinhaService.java
@@ -0,0 +1,21 @@
+package com.faculdade.albumcopa.service;
+
+
+import com.faculdade.albumcopa.model.Figurinha;
+import com.faculdade.albumcopa.repository.FigurinhaRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class FigurinhaService {
+
+	private final FigurinhaRepository figurinhaRepository;
+	
+	public FigurinhaService(FigurinhaRepository figurinhaRepository)	{
+		this.figurinhaRepository = figurinhaRepository;
+	}
+	
+	public List<Figurinha> listarTodas()	{
+		return figurinhaRepository.findAll();
+	}
+}
